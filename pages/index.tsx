cb796a76b0c4b1833beae32b581930767f744bdb
Erik Pizana <erikpzcanedo@outlook.com>
2022-01-07 17:55:18 -0600

    Title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,12 @@ const Home: NextPage = () => {
   return (
     <HomeContainer>
       <Head>
+        <title>ErikDev.</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ProgressBar />
       <NavBar open={openMenu} handleOpen={setopenMenu} />
